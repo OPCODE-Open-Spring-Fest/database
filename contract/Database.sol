a05275aa016e71233b2e60228008bac751482ad4
x256 <153709773+xTwo56@users.noreply.github.com>
2024-03-30 20:18:03 +0530

    feat: created a function to check details

--- contract/Database.sol
+++ contract/Database.sol
@@ -1,39 +1,38 @@
+// SPDX-License-Identifier: MI
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0<0.9.0;
 
 contract Database{
-
     struct Details {
         string aadharId;
         string name;
         string DOB;
         string phoneNo;
-        uint rollNo;
-        uint batchNo;
+        string rollNo;
+        string batchNo;
 
     }
     
-    mapping( address => Details) public list;
+    mapping( address => Details) private list;
 
-    uint256 public count=0;
+    uint256 private count=0;
     constructor()
     {
 
     }
 
     modifier personPresent{
-        require(list[msg.sender].rollNo==0, "User doesn't exist");
+        require(keccak256(abi.encodePacked(list[msg.sender].aadharId)) != keccak256(abi.encodePacked("")), "Person doesn't exist");
         _;
     }
-
-    function addPerson(string calldata aadharId,string calldata name, string calldata DOB, string calldata phoneNo, uint rollNo, uint batchNo) public 
+    function addPerson(string memory aadharId,string memory name, string memory DOB, string memory phoneNo, string memory rollNo, string memory batchNo) public 
     {
-        Details storage person = Details({aadharId: aadharId,name: name,DOB: DOB,phoneNo: phoneNo, rollNo: rollNo, batchNo: batchNo});
+        Details memory person = Details({aadharId: aadharId,name: name,DOB: DOB,phoneNo: phoneNo, rollNo: rollNo, batchNo: batchNo});
         list[msg.sender]=person;
         count++;
     }
-    
-    function updateDetails(string calldata aadharId,string calldata name, string calldata DOB, string calldata phoneNo, uint rollNo, uint batchNo)public personPresent{
+
+    function updateDetails(string memory aadharId,string memory name, string memory DOB, string memory phoneNo, string memory rollNo, string memory batchNo)public personPresent{
         Details storage person = list[msg.sender];
         person.aadharId = aadharId;
         person.name = name;
@@ -42,8 +41,11 @@ contract Database{
         person.rollNo = rollNo;
         person.batchNo = batchNo;
     }
-
-
-
+    function checkDetails()public personPresent view returns(string[6] memory){
+        Details memory person = list[msg.sender];
+        string[6] memory details = [person.aadharId, person.name, person.DOB, person.phoneNo, person.rollNo, person.batchNo];
+        return details;
+    }
 
 }
+
