b89d0acba05cd2664c9e0c54192ef93fc775037e
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2024-04-01 00:38:11 +0530

    feat: removed unecessary functions

--- contract/Database.sol
+++ contract/Database.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MI
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0<0.9.0;
 
@@ -21,10 +20,6 @@ contract Database{
 
     }
 
-    modifier personPresent{
-        require(keccak256(abi.encodePacked(list[msg.sender].aadharId)) != keccak256(abi.encodePacked("")), "Person doesn't exist");
-        _;
-    }
     function addPerson(string memory aadharId,string memory name, string memory DOB, string memory phoneNo, string memory rollNo, string memory batchNo) public 
     {
         Details memory person = Details({aadharId: aadharId,name: name,DOB: DOB,phoneNo: phoneNo, rollNo: rollNo, batchNo: batchNo});
@@ -32,20 +27,6 @@ contract Database{
         count++;
     }
 
-    function updateDetails(string memory aadharId,string memory name, string memory DOB, string memory phoneNo, string memory rollNo, string memory batchNo)public personPresent{
-        Details storage person = list[msg.sender];
-        person.aadharId = aadharId;
-        person.name = name;
-        person.DOB = DOB;
-        person.phoneNo = phoneNo;
-        person.rollNo = rollNo;
-        person.batchNo = batchNo;
-    }
-    function checkDetails()public personPresent view returns(string[6] memory){
-        Details memory person = list[msg.sender];
-        string[6] memory details = [person.aadharId, person.name, person.DOB, person.phoneNo, person.rollNo, person.batchNo];
-        return details;
-    }
 
 }
 
