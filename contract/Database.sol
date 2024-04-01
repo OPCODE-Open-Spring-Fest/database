1df6da51a7bbd143bb436a10c0fd8dd520f9bb4e
x256 <153709773+xTwo56@users.noreply.github.com>
2024-04-01 22:25:05 +0530

    feat: created a function to find user by their aadharId

--- contract/Database.sol
+++ contract/Database.sol
@@ -10,65 +10,89 @@ contract Database{
         string phoneNo;
         string rollNo;
         string batchNo;
-
     }
 
-        
-}
-
-    
-    mapping( uint256=> Details) public list;
-    mapping (string => bool) public added;
+    mapping( address => Details) public list;
+    mapping( string => bool) public added;
+    mapping( string => Details) private aadharToUser;
+ 
     uint256 public count=0;
     address public admin;
     bool alreadyset=false;
+
     constructor()
     {
 
     }
-
-
-    function addPerson(string memory aadharId,string memory name, string memory DOB, string memory phoneNo, string memory rollNo, string memory batchNo) public 
 
     modifier Added (string memory aadhar) 
     {
         require(!added[aadhar],"Details already added");
         _;
     }
+
+    modifier personPresent{
+        require(keccak256(abi.encodePacked(list[msg.sender].aadharId)) != keccak256(abi.encodePacked("")), "User doesn't exist");
+        _;
+    }
+
     modifier onlyOnce()
     {
         require(!alreadyset,"Admin is already set");
         _;
     }
+
     modifier onlyAdmin()
     {
         require(msg.sender==admin,"Only admin can call this function");
         _;
     }
+
     function set_Admin( address _admin) public onlyOnce()
     {
         admin=_admin;
         alreadyset=true;
     }
+
     function changeAdmin(address newadmin) public onlyAdmin()
     {
         admin=newadmin;
+    }
 
+    function getAdress()external view returns(address){
+        return msg.sender;
     }
     
-    function addPerson(string calldata aadharId,string calldata name, string calldata DOB, string calldata phoneNo ) public Added (aadharId)
-
+    function addPerson(string memory aadharId,string memory name, string memory DOB, string memory phoneNo, string memory rollNo, string memory batchNo) public 
     {
         Details memory person = Details({aadharId: aadharId,name: name,DOB: DOB,phoneNo: phoneNo, rollNo: rollNo, batchNo: batchNo});
-        list[count]=person;
-        added[aadharId]=true;
+        list[msg.sender]=person;
+        added[aadharId] = true;
+        aadharToUser[aadharId] = person;
         count++;
     }
 
+    function updateDetails(string memory aadharId,string memory name, string memory DOB, string memory phoneNo, string memory rollNo, string memory batchNo)public personPresent{
+        Details storage person = list[msg.sender];
+        person.aadharId = aadharId;
+        person.name = name;
+        person.DOB = DOB;
+        person.phoneNo = phoneNo;
+        person.rollNo = rollNo;
+        person.batchNo = batchNo;
+    }
+    function checkDetails()public personPresent view returns(string[6] memory){
+        Details memory person = list[msg.sender];
+        string[6] memory details = [person.aadharId, person.name, person.DOB, person.phoneNo, person.rollNo, person.batchNo];
+        return details;
+    }
+
+    function findByAadhaarId(string memory _aadharId)public view onlyAdmin returns(string[6] memory){
+        require((bytes(aadharToUser[_aadharId].aadharId).length > 0), "Not found");
+        Details memory person = aadharToUser[_aadharId];
+        string[6] memory details = [person.aadharId, person.name, person.DOB, person.phoneNo, person.rollNo, person.batchNo];
+        return details;
+    }
     
-
-
-
-
-
 }
+
